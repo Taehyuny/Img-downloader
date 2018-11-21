158ef0dcb3aa0be6915994a17193d49e6fa3bf12
GreenTCH <corkdwns@gmail.com>
2018-11-21 11:44:20 +0900

    1차 수정

--- Image Downloader.py
+++ Image Downloader.py
@@ -8,7 +8,6 @@ def get(max_count = 35):
         img_url2 = img_url + tempcount  + ".jpg"
         print("+-----[ %d번 째 이미지 ]-----+" %count)
         print("+-----[ 주소 : %s ]-----+" %img_url2)
-        #urllib.request.urlretrieve(img_url2.)
         r = requests.get(img_url2, stream = True, headers = {'User-agent':'Mozilla/5.0'})
         if r.status_code == 200:
             with open('img_test/%s.jpg' % tempcount, 'wb') as f:
